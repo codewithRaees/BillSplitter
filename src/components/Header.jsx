732dfe07a8161e5032d6ec0cfba7d986045137f7
Raees <mraiskundi@gmail.com>
2024-02-15 21:21:34 +0500

    15Feb2024

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="max-w-[1120px] bg-purple-200 flex justify-center">
+        <h1>Bill Splitter</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
