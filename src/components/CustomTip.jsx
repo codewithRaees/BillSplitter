6423e85996de0ceacd7aed2dd10b8315f97602b3
Raees <mraiskundi@gmail.com>
2024-02-17 08:10:25 +0500

    17Feb2024

--- src/components/CustomTip.jsx
+++ src/components/CustomTip.jsx
@@ -8,7 +8,7 @@ const CustomTip = () => {
             type="text"
             name="price"
             id="customtip"
-            className="block w-full rounded-md border-0 py-1.5 pl-2 pr-12 text-purple-900 ring-1 ring-inset ring-purple-500 placeholder:text-purple-300 focus:outline-purple-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md text-sm border-0 py-1.5 pl-2 pr-12 text-purple-900 ring-1 ring-inset ring-purple-500 placeholder:text-purple-300 focus:outline-purple-600 sm:text-sm sm:leading-6"
             placeholder="Custom Tip"
           />
         </div>
