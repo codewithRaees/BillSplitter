e3f388f6520f654f1092767361140309b8c57feb
Raees <mraiskundi@gmail.com>
2024-02-23 22:21:02 +0500

    23Feb2024

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = ({ title }) => {
   return (
     <div>
-      <div className="py-4 mt-4 font-bold text-purple-800  shadow-sm  bg-purple-200 m-auto flex justify-center">
+      <div className="py-4 mt-4 font-bold text-purple-800  shadow-sm  bg-purple-200  flex justify-center">
         <p className="">{title}</p>
       </div>
     </div>
